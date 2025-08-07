bacdeb813e7c6b826d9b2683ef4ee891a82efff1
momose_souya <cangyebailai@gmail.com>
2025-08-07 20:51:44 +0900

    [#10252] スクロール機能の実装
    バッグ内でスクロールビューを追加

--- Assets/Scripts/Home/Bag/ChoiceWeaponDataManager.cs
+++ Assets/Scripts/Home/Bag/ChoiceWeaponDataManager.cs
@@ -12,12 +12,20 @@ public class ChoiceWeaponDataManager : WeaponBase
 
     [SerializeField] TextMeshProUGUI weaponNameText;
     [SerializeField] GameObject detailWeaponBack, reinforceWeaponBack;
+    [SerializeField] Button reinforceButton; // 強化ボタン
+
+    private void Awake()
+    {
+        reinforceButton.interactable = false;
+    }
 
     // 武器が選択された時、強化のために武器の基本データを保存する
     public void SetChoiceWeaponData(int id, int level)
     {
         weapon_id = id;
         weapon_level = level;
+
+        reinforceButton.interactable = true;
     }
 
     // 詳細画面の設定、強化ボタンが押された時に画像等が変わる(武器)
